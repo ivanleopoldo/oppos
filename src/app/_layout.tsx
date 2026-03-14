877c216287e9b406fc07d751bb2bb56bc7db4b32
ivanleopoldo <ivanleopoldoc@gmail.com>
2026-03-14 23:35:41 +0800

    chore: adds react-native-reusables

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,29 @@
 import "@/global.css";
+
+import { NAV_THEME } from "@/lib/theme";
+import { ThemeProvider } from "@react-navigation/native";
+import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
+import { useColorScheme } from "nativewind";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+	const { colorScheme } = useColorScheme();
+
 	return (
-		<SafeAreaProvider>
-			<Stack />
-		</SafeAreaProvider>
+		<ThemeProvider value={NAV_THEME[colorScheme === "dark" ? "dark" : "light"]}>
+			<SafeAreaProvider>
+				<RootLayoutNav />
+			</SafeAreaProvider>
+		</ThemeProvider>
 	);
 }
+
+const RootLayoutNav = () => {
+	return (
+		<>
+			<Stack />
+			<PortalHost />
+		</>
+	);
+};
